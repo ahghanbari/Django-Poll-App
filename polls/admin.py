6ec83f50bf683ea4ee75bf1c925ec2e1cf8b6e16
A.H Ghanbari <ahghanbari@protonmail.com>
2026-06-18 17:38:37 +0330

    Adding Polls, Choice and Vote to admin panel

--- polls/admin.py
+++ polls/admin.py
@@ -1,22 +1,33 @@
 from django.contrib import admin
-from .models import Choice, Question
+from .models import Poll, Choice, Vote
 
 
-class ChoiceInline(admin.TabularInline):
+class ChoiceInline(admin.TabularInline):  # or admin.StackedInline for a different layout
     model = Choice
-    extra = 3
+    extra = 1
+
+class VoteInline(admin.TabularInline):  # or admin.StackedInline for a different layout
+    model = Vote
+    extra = 1
+
+@admin.register(Poll)
+class PollAdmin(admin.ModelAdmin):
+    list_display = ["text", "owner", "pub_date", "active", "created_at"]
+    search_fields = ["text", "owner__username"]
+    list_filter = ["active", 'created_at', 'pub_date']
+    date_hierarchy = "pub_date"
+    inlines = [ChoiceInline, VoteInline]
 
 
-class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
-    ]
-    inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+@admin.register(Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["choice_text", "poll", 'created_at', 'updated_at']
+    search_fields = ["choice_text", "poll__text"]
+    autocomplete_fields = ["poll"]
 
 
-admin.site.register(Question, QuestionAdmin)
-admin.site.register(Choice)
+@admin.register(Vote)
+class VoteAdmin(admin.ModelAdmin):
+    list_display = ["choice", "poll", "user", 'created_at']
+    search_fields = ["choice__choice_text", "poll__text", "user__username"]
+    autocomplete_fields = ["choice", "poll", "user"]
